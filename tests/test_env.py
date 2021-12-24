4c6cf7d856188414c349438ebf8004cbe9bbb914
pedrobzz <pedrobzz66@gmail.com>
2021-12-24 15:27:03 -0300

    test(Env Test): Added simple tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,5 +1,18 @@
-from env import __version__
+from env import __version__, Env
+import pytest
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.parametrize(
+    "test_input,expected", 
+    [
+        ("login", "admin"), 
+        ("password", "admin12345"), 
+        ("server_url", "256.23.0.143/3001/api")
+    ])
+def test_env(test_input, expected):
+    env = Env("./tests/tokens").initialize()
+
+    assert env[test_input] == expected
